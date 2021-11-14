cc64e5018418ff672346da6b92004c036056669d
ame diphoko <diphokoame@gmail.com>
2021-11-14 09:41:16 +0200

    add navbar dropdown item, group operational reports link

--- esr21_reports/navbars.py
+++ esr21_reports/navbars.py
@@ -2,6 +2,8 @@ from django.conf import settings
 
 from edc_navbar import NavbarItem, site_navbars, Navbar
 
+from .navbar_dropdown_item import NavBarDropdownItem
+
 esr21_reports = Navbar(name='esr21_reports')
 
 no_url_namespace = True if settings.APP_NAME == 'esr21_reports' else False
@@ -12,23 +14,16 @@ esr21_reports.append_item(
                fa_icon='fa-cogs',
                url_name='esr21_reports:esr21_reports_home_url'))
 
-esr21_reports.append_item(
-    NavbarItem(name='Screening Reports',
-               label='Screening Reports',
-               fa_icon='fa-cogs',
-               url_name='esr21_reports:esr21_screening_reports_url'))
+operational_reports = NavBarDropdownItem(
+    name='operational_reports',
+    label='Operational Reports',
+    fa_icon='fa-cogs',
+    url_name='#',
+    dropdown_items={'Screening Reports': 'esr21_reports:esr21_screening_reports_url',
+                    'Consent Reports': 'esr21_reports:esr21_consent_reports_url',
+                    'Vaccination Reports': 'esr21_reports:esr21_vaccine_reports_url'})
 
-esr21_reports.append_item(
-    NavbarItem(name='Consent Reports',
-               label='Consent Reports',
-               fa_icon='fa-cogs',
-               url_name='esr21_reports:esr21_consent_reports_url'))
-
-esr21_reports.append_item(
-    NavbarItem(name='Vaccination Reports',
-               label='Vaccination Reports',
-               fa_icon='fa-cogs',
-               url_name='esr21_reports:esr21_vaccine_reports_url'))
+esr21_reports.append_item(operational_reports)
 
 esr21_reports.append_item(
     NavbarItem(name='Adverse Events Reports',
