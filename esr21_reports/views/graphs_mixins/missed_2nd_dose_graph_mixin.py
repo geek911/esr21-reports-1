b7112686ca3809aa2993e064c3d8a61c52e84e6d
Coulson Kgathi <ckgathi@gmail.com>
2022-02-14 13:40:27 +0200

    refactor missed dose

--- esr21_reports/views/graphs_mixins/missed_2nd_dose_graph_mixin.py
+++ esr21_reports/views/graphs_mixins/missed_2nd_dose_graph_mixin.py
@@ -43,29 +43,26 @@ class Missed2ndDoseGraphMixin(EdcBaseViewMixin):
             "others": 0
         }
 
-        for appt in self.appointment_cls.objects.filter(visit_code='1070'):
+        second_dose = self.vaccination_details_cls.objects.filter(
+                    subject_visit__visit_code='1070',
+                    received_dose_before='second_dose').values_list('subject_visit__subject_identifier', flat=True)
+        second_dose = list(set(second_dose))
+        for appt in self.appointment_cls.objects.filter(visit_code='1070').exclude(subject_identifier__in=second_dose):
             latest_start = (appt.timepoint_datetime + appt.visits.get(
                 appt.visit_code).rupper).astimezone(pytz.timezone('Africa/Gaborone'))
-            try:
-                self.vaccination_details_cls.objects.get(
-                    subject_visit__subject_identifier=appt.subject_identifier,
-                    subject_visit__visit_code='1070',
-                    received_dose_before='second_dose'
-                )
-            except self.vaccination_details_cls.DoesNotExist:
-                if latest_start.date() < get_utcnow().date():
-                    site_statistics[appt.site_id] += 1
-                    consent = self.consent_cls.objects.filter(
-                        subject_identifier=appt.subject_identifier).last()
-                    if consent:
-                        if consent.gender == 'F':
-                            missed_second_dose_by_gender['females'] += 1
-                        elif consent.gender == 'M':
-                            missed_second_dose_by_gender['males'] += 1
-                        elif consent.gender == 'OTHER':
-                            missed_second_dose_by_gender['others'] += 1
-                    else:
-                        print(appt.subject_identifier, '**************')
+            if latest_start.date() < get_utcnow().date():
+                site_statistics[appt.site_id] += 1
+                consent = self.consent_cls.objects.filter(
+                    subject_identifier=appt.subject_identifier).last()
+                if consent:
+                    if consent.gender == 'F':
+                        missed_second_dose_by_gender['females'] += 1
+                    elif consent.gender == 'M':
+                        missed_second_dose_by_gender['males'] += 1
+                    elif consent.gender == 'OTHER':
+                        missed_second_dose_by_gender['others'] += 1
+                else:
+                    print(appt.subject_identifier, '**************')
         site_statistics_list = list(map(list, site_statistics.items()))
         return [site_statistics_list, missed_second_dose_by_gender]
 
