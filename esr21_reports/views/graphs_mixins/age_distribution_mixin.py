c7ab9bad8c5f6a9a42899f3dc785239568ae1b72
MoffatMore <mofenyimoffat@gmail.com>
2022-06-13 15:41:48 +0200

    add models folder

--- esr21_reports/views/graphs_mixins/age_distribution_mixin.py
+++ esr21_reports/views/graphs_mixins/age_distribution_mixin.py
@@ -108,8 +108,7 @@ class AgeDistributionGraphMixin(EdcBaseViewMixin):
             min = np.min(max_ages)
             max = np.max(min_ages)
             return [min,lowerquartile,median,upperquartile,max]
-        
-           
+
     def get_site_id(self, site_name_postfix):
         try:
             return Site.objects.get(name__endswith=site_name_postfix).id
