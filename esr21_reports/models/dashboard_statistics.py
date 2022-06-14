927f34344929c1c14c0d6b8c9c0dfa34fff750dd
MoffatMore <mofenyimoffat@gmail.com>
2022-06-14 15:55:04 +0200

    add dasboard statics

--- esr21_reports/models/dashboard_statistics.py
+++ esr21_reports/models/dashboard_statistics.py
@@ -1,8 +1,7 @@
 from django.db import models
-import json
 from ..choices import STATISTICS_TYPE
+
 
 class DashboardStatistics(models.Model):
     key = models.CharField(max_length=50)
     value = models.TextField()
-    
