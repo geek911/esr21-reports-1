14c6bc467f6974086c1a59113f57ed0a6b66eefe
Moses Chawawa <moseschawawa@gmail.com>
2022-06-14 11:05:17 +0200

    update the model to use json

--- esr21_reports/models/dashboard_statistics.py
+++ esr21_reports/models/dashboard_statistics.py
@@ -4,5 +4,5 @@ from ..choices import STATISTICS_TYPE
 
 class DashboardStatistics(models.Model):
     key = models.CharField(max_length=50)
-    value = models.CharField(max_length=500)
+    value = models.TextField()
     
